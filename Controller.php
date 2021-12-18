3c7b316116bba1961bede39ab02c83892b61c7f3
Amit Roy <uamitroy@gmail.com>
2021-12-18 16:59:42 +0530

    setup

---
+++ Controller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author      Amit Roy <amit@softflies.com>
+ * @copyright   Copyright (c), 2021 Amit Roy
+ * @license     MIT public license
+ */
+namespace Inculcate\Controllers;
+
+use Inculcate\View;
+/**
+ * trait Controller.
+ */
+trait Controller
+{   
+	
+
+} 
